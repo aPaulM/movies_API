d9d1043f416e85b91450a0f12af3982a1dfe23ae
Aiden McPeters <aiden.lona777@gmail.com>
2023-06-26 16:51:16 -0400

    adding Movie and Review models

---
+++ src/main/java/models/Movie.java
@@ -0,0 +1,29 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // comes from lombok...
+// allows us not to have to generate/code the getters, setters, and toString methods.
+@AllArgsConstructor // comes from lombok... is self explanatory.
+@NoArgsConstructor // comes from lombok... also is self explanatory.
+public class Movie {
+
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+}
