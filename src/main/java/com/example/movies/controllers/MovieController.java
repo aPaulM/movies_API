42000a161304428e33895da432e2ead3462c10d2
Aiden McPeters <aiden.lona777@gmail.com>
2023-06-27 20:26:29 -0400

    added listAllMovies method

--- src/main/java/com/example/movies/controllers/MovieController.java
+++ src/main/java/com/example/movies/controllers/MovieController.java
@@ -1,17 +1,26 @@
 package com.example.movies.controllers;
 
+import com.example.movies.models.Movie;
+import com.example.movies.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    @Autowired // Super Busted way of Instantiating this service.
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> getAllMovies() {
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.listAllMovies(), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/movies/service/MovieService.java
+++ src/main/java/com/example/movies/service/MovieService.java
@@ -1,7 +1,25 @@
 package com.example.movies.service;
 
+import com.example.movies.models.Movie;
+import com.example.movies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService {
+
+    @Autowired // @Autowired is a super busted way of instantiating this repository.
+    private MovieRepository movieRepo;
+
+  // CONSTRUCTOR not needed because of @Autowired
+  // public MovieService(MovieService movieService) {
+  //     this.movieService = movieService;
+  // }
+
+    public List<Movie> listAllMovies() {
+        return movieRepo.findAll();
+    }
+
 }
