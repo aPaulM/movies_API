4b5dcd405815fe472abaade16dd8ffe72279eb81
Aiden McPeters <aiden.lona777@gmail.com>
2023-06-27 20:45:40 -0400

    added a repository method called findMovieByImdbId and added service and controller methods to get a single movie

--- src/main/java/com/example/movies/controllers/MovieController.java
+++ src/main/java/com/example/movies/controllers/MovieController.java
@@ -2,14 +2,17 @@ package com.example.movies.controllers;
 
 import com.example.movies.models.Movie;
 import com.example.movies.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -23,4 +26,8 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.listAllMovies(), HttpStatus.OK);
     }
 
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> GetSingleMovieById(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.listSingleMovie(imdbId), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/movies/service/MovieService.java
+++ src/main/java/com/example/movies/service/MovieService.java
@@ -2,10 +2,12 @@ package com.example.movies.service;
 
 import com.example.movies.models.Movie;
 import com.example.movies.repository.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -20,6 +22,10 @@ public class MovieService {
 
     public List<Movie> listAllMovies() {
         return movieRepo.findAll();
+    }
+
+    public Optional<Movie> listSingleMovie(String imdbId) {
+        return movieRepo.findMovieByImdbId(imdbId);
     }
 
 }
